5a307ff5c81890e664be5045b6897dc8ea7a0e20
Anthon Liljeroth <49619480+liljeroth@users.noreply.github.com>
2021-12-02 07:59:56 +0100

    Day 2

---
+++ day2/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+# Day 2
+# 1: 1638335023 (3m 50s)
+# 2: 1638335238 (5m 54s)
+
+# Read input file
+day_file = open('input','r')
+c = day_file.read().split('\n')[:-1]
+
+# Solve part 1
+position = 0
+depth = 0
+for i in range(0, len(c)):
+	direction = c[i].split(' ')[0]
+	steps = int(c[i].split(' ')[1])
+	
+	if direction == 'forward':
+		position += steps
+	elif direction == 'down':
+		depth += steps
+	elif direction == 'up':
+		depth -= steps
+
+print('Answer part 1: ', position*depth)
+
+# Solve part 2
+position = 0
+depth = 0
+aim = 0
+for i in range(0, len(c)):
+	direction = c[i].split(' ')[0]
+	steps = int(c[i].split(' ')[1])
+	
+	if direction == 'forward':
+		position += steps
+		depth += aim * steps
+	elif direction == 'down':
+		aim += steps
+	elif direction == 'up':
+		aim -= steps
+
+print('Answer part 2: ', position*depth)
